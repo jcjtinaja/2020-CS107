ce634386b22db6d128632ed9d6c20f5774211863
jcjtinaja <jan.carlo.tinaja@gmail.com>
2021-06-08 15:44:58 +0800

    Fixed bugs for exercise_1.sh

--- pair_programming/PP2/exercise_1.sh
+++ pair_programming/PP2/exercise_1.sh
@@ -3,8 +3,9 @@
 read -r -p 'File to commit: ' file
 git add $file
 git status
+echo "'$file' staged successfully"
 
-read -r -p 'Do you wish to commit? ' varCommit
+read -r -p 'Do you wish to commit? (Y/N): ' varCommit
 
 if [ $varCommit == 'N' ]; then
 	git restore --staged $file
@@ -13,16 +14,16 @@ elif [ $varCommit == 'Y' ]; then
 	read -r -p 'Enter commit message: ' msgCommit
 	git commit -m $msgCommit
 	git status
+	echo "'$file' committed successfully"
 fi
 
-echo 'Do you wish to push?'
-read varPush
+read -r -p 'Do you wish to push? (Y/N): ' varPush
 
-if ['$varPush'='N']; then
+if [ $varPush == 'N' ]; then
 	git reset --soft HEAD^
 	git restore --staged $file
 	exit 1
-elif ['$varPush'='Y']; then
+elif [ $varPush == 'Y' ]; then
 	echo "Trying to push '$varCommit'..."
 	git push
 fi
